66982ec75bb95ba86bb487e15b011ceaf9cffb88
tuananh-2005 <doanhhavana111@gmail.com>
2025-03-27 00:54:06 +0700

    Asset

---
+++ Assets/Script/CameraFollow2D.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    private Vector3 startOffset; // Lưu khoảng cách ban đầu
+    public Transform target; // Nhân vật cần theo dõi
+    public float smoothSpeed = 2f; // Giảm tốc độ mượt
+    public float followThreshold = 0.5f; // Khoảng cách tối thiểu trước khi camera di chuyển
+
+    void Start()
+    {
+        if (target != null)
+        {
+            startOffset = transform.position - target.position; // Lưu khoảng cách ban đầu
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + startOffset;
+            float distance = Vector3.Distance(transform.position, desiredPosition);
+
+            // Khi nhân vật di chuyển xa một khoảng nhất định, camera di chuyển chậm dần về vị trí mới
+            if (distance > followThreshold)
+            {
+                transform.position = Vector3.Lerp(transform.position, desiredPosition, (smoothSpeed * distance) * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Animator animator;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpForce = 8f;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            animator.ResetTrigger("Jump");
+        }
+
+        float moveZ = Input.GetAxis("Vertical");
+        if (moveZ != 0)
+        {
+            transform.rotation = Quaternion.Euler(0, moveZ > 0 ? 0 : 180, 0);
+        }
+
+        Vector3 move = transform.forward * Mathf.Abs(moveZ);
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        animator.SetFloat("Speed", Mathf.Abs(moveZ));
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = jumpForce;
+            animator.SetTrigger("Jump");
+        }
+    }
+}
